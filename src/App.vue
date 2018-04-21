d03d547638aa5b6e4017b9b1fcbafba365369eac
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-21 09:28:40 +0700

    add loaders.css for use but not improve

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <Spinner name="circle" />
-    <Spinner name="cube-grid" color="steelblue"/>
-    <Spinner name="wave" color="purple"/>
+    <div :key="spin" v-for="spin in allSpinner" class="loading">
+      {{spin}}
+      <Spinner :name="spin"/>
+    </div>
   </div>
 </template>
 
 <script>
 import Spinner from './components/Spinner'
+import spinners from './components/spinners'
 
 export default {
   name: 'App',
   components: {
     Spinner
+  },
+  data () {
+    return {
+      allSpinner: Object.keys(spinners.allSpinners)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.loading {
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  margin: 50px;
 }
 </style>
--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,14 +1,14 @@
 <template>
   <div
   v-if="loading.className !== ''"
-  :class="loading.className"
+  :class="'sk-fade-in sk-spinner ' + loading.className"
   :style="color !== ''? 'color: '+ color+ ';': ''">
     <div :key="index" v-for="(_, index) in loading.divCount"></div>
   </div>
 </template>
 
 <script>
-
+import spinners from './spinners'
 export default {
   name: 'Spinner',
   props: {
@@ -23,50 +23,18 @@ export default {
   },
   data () {
     return {
-      spinkitSpinners: {
-        'circle': { className: 'sk-circle', divCount: 12 },
-        'cube-grid': { className: 'sk-cube-grid', divCount: 9 },
-        'wave': { className: 'sk-wave', divCount: 5 },
-        'folding-cube': { className: 'sk-folding-cube', divCount: 4 },
-        'three-bounce': { className: 'sk-three-bounce', divCount: 3 },
-        'double-bounce': { className: 'sk-double-bounce', divCount: 2 },
-        'wandering-cubes': { className: 'sk-wandering-cubes', divCount: 2 },
-        'chasing-dots': { className: 'sk-chasing-dots', divCount: 2 },
-        'rotating-plane': { className: 'sk-rotating-plane', divCount: 1 },
-        'pulse': { className: 'sk-pulse', divCount: 1 },
-        'wordpress': { className: 'sk-wordpress', divCount: 1 }
-      },
-      loadersCssSpinners: {
-        'ball-grid-beat': { divCount: 9 },
-        'ball-grid-pulse': { divCount: 9 },
-        'line-spin-fade-loader': { divCount: 8 },
-        'ball-spin-fade-loader': { divCount: 8 },
-        'ball-pulse-rise': { divCount: 5 },
-        'line-scale': { divCount: 5 },
-        'line-scale-pulse-out': { divCount: 5 },
-        'line-scale-pulse-out-rapid': { divCount: 5 },
-        'pacman': { divCount: 5 },
-        'line-scale-party': { divCount: 4 },
-        'ball-triangle-path': { divCount: 3 },
-        'ball-scale-multiple': { divCount: 3 },
-        'ball-scale-ripple-multiple': { divCount: 3 },
-        'ball-pulse-sync': { divCount: 3 },
-        'ball-beat': { divCount: 3 },
-        'ball-zig-zag': { divCount: 2 },
-        'ball-zig-zag-deflect': { divCount: 2 },
-        'ball-clip-rotate-pulse': { divCount: 2 },
-        'ball-clip-rotate-multiple': { divCount: 2 },
-        'ball-clip-rotate': { divCount: 1 },
-        'ball-scale-ripple': { divCount: 1 },
-        'triangle-skew-spin': { divCount: 1 }
-      }
+      allSpinners: spinners.allSpinners
     }
   },
   computed: {
     loading () {
-      const spinkit = this.spinkitSpinners[this.name]
-      if (spinkit !== undefined) {
-        return spinkit
+      const allSpinners = this.allSpinners[this.name]
+      if (allSpinners !== undefined) {
+        const className = allSpinners.className !== undefined ? allSpinners.className : this.name
+        return {
+          ...allSpinners,
+          className
+        }
       } else {
         return {
           className: '',
@@ -79,6 +47,7 @@ export default {
 </script>
 
 <style scoped>
+@import 'loaders.css';
 @import './css/base.css';
 @import './css/loaders-css.css';
 @import './css/fade-in.css';
