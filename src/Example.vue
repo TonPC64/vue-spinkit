04e0de6cd27f94c82683afefbfc27369e453e6ac
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-24 14:21:02 +0700

    update example page

---
+++ src/Example.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="app">
+    <h1>Example Vue Spinkit</h1>
+    <div :key="spinner.name" v-for="spinner in allSpinner">
+      <h2>{{spinner.name}}</h2>
+      <div class="columns">
+        <div :key="spin" v-for="(spin, index) in spinner.spin" class="loading column">
+          <span class="example-code" v-html="showExam(spin, color[index])"></span>
+          <Spinner :name="spin" :color="color[index]"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Spinner from './components/Spinner'
+import spinners from './components/spinners'
+import randomColor from 'randomcolor'
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+
+export default {
+  name: 'App',
+  components: {
+    Spinner
+  },
+  data () {
+    return {
+      allSpinner: [
+        {
+          name: 'Spinkit',
+          spin: Object.keys(spinners.spinkitSpinners)
+        },
+        {
+          name: 'Loaders.css',
+          spin: Object.keys(spinners.loadersCssSpinners)
+        }
+      ],
+      color: randomColor({
+        count: Object.keys(spinners.allSpinners).length
+      })
+    }
+  },
+  methods: {
+    showExam (name, color = '') {
+      const newcolor = color !== '' ? `color="${color}"` : ''
+      const code = `<Spinner name="${name}" ${newcolor}/>`
+      return Prism.highlight(code, Prism.languages.markup, 'markup')
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  left: 0;
+  right: 0;
+  margin: auto;
+  margin-top: 60px;
+  width: 100%;
+}
+.loading {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  margin-bottom: 50px;
+}
+.example-code {
+  padding: 10px;
+  width: 90vw;
+  border-radius: 3px;
+  background-color: rgb(224, 224, 224);
+  margin: 5px 5px 20px 5px;
+}
+
+.columns {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: row;
+   flex-wrap: wrap;
+   flex-flow: row wrap;
+   align-content: flex-end;
+}
+.column {
+  display: flex;
+  flex: 1;
+}
+@media screen and (min-width: 1024px) {
+  #app {
+    width: 1024px;
+  }
+  .example-code {
+     width: 450px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Example from './Example'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components: { Example },
+  template: '<Example/>'
 })
