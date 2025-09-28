d0ebf580730a7232bf1c2bd267f060c0143e45ed
chanwit-p <chanwit.p@linecorp.com>
2025-09-28 15:58:46 +0700

    fix: npm ci

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
-let createVuePlugin
-try {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const mod = require('vite-plugin-vue2')
-  createVuePlugin = mod && (mod.createVuePlugin || mod.default && mod.default.createVuePlugin) ? (mod.createVuePlugin || mod.default.createVuePlugin) : null
-} catch (e) {
-  // eslint-disable-next-line no-console
-  console.error('Could not require vite-plugin-vue2', e)
-}
+import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
-  plugins: [createVuePlugin && createVuePlugin()],
+  plugins: [createVuePlugin()],
   build: {
     outDir: 'build',
     lib: {
