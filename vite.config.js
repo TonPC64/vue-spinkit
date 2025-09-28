84fcc1c537129cf4f256fd98cefa32f80a321fbf
chanwit-p <chanwit.p@linecorp.com>
2025-09-28 19:57:26 +0700

    fix: inject css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [createVuePlugin(), cssInjectedByJsPlugin()],
   css: {
     postcss: {
       plugins: [
